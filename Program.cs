98b654186543dee3aa3871298787abc1c60ccc71
Nayeema-Tasnim <124617804+Nayeema-Tasnim@users.noreply.github.com>
2025-04-04 16:26:26 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews(); // Enables MVC support
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";//extra
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");//extra
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
